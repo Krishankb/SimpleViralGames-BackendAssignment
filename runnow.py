70248ceb096385d137ca7de4965bbc46b002a49e
Krishankb <30771097+Krishankb@users.noreply.github.com>
2023-03-07 20:03:45 +0530

    Update runnow.py

--- runnow.py
+++ runnow.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
@@ -12,7 +12,7 @@ class Game(db.Model):
     name = db.Column(db.String(50), nullable=False)
     url = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(50), nullable=False)
-    published_date = db.Column(db.DateTime, nullable=False)
+    published_date = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f'<Game {self.name}>'
@@ -22,9 +22,9 @@ if __name__ == '__main__':
         db.create_all()
 
         # Add test data
-        game1 = Game(name='Super Mario', url='https://example.com/super-mario', author='Nintendo', published_date=datetime.strptime('2022-01-01 12:00:00', '%Y-%m-%d %H:%M:%S'))
-        game2 = Game(name='Minecraft', url='https://example.com/minecraft', author='Mojang', published_date=datetime.strptime('2022-02-01 12:00:00', '%Y-%m-%d %H:%M:%S'))
-        game3 = Game(name='Fortnite', url='https://example.com/fortnite', author='Epic Games', published_date=datetime.strptime('2022-03-01 12:00:00', '%Y-%m-%d %H:%M:%S'))
+        game1 = Game(name='Super Mario', url='https://example.com/super-mario', author='Nintendo', published_date='2022-01-01 12:00:00')
+        game2 = Game(name='Minecraft', url='https://example.com/minecraft', author='Mojang', published_date='2022-02-01 12:00:00')
+        game3 = Game(name='Fortnite', url='https://example.com/fortnite', author='Epic Games', published_date='2022-03-01 12:00:00')
 
         db.session.add_all([game1, game2, game3])
         db.session.commit()
